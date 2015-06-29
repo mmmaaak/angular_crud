3f646402fa37b90dbe770c58780323063fb6d3c6
Pavel Shvedov <pavel.shvedov@tstn.ru>
2015-06-29 12:11:44 +0300

    Gulp plugins and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+    react = require('gulp-react'),
+    concat = require('gulp-concat');
+
+gulp.task('build', function() {
+   return gulp.src([
+       './src/js/crud.table.component.js',
+       './src/js/crud.create.component.js',
+       './src/js/crud.update.component.js',
+       './src/js/crud.delete.component.js',
+   ])
+   .pipe(react())
+   .pipe(uglify())
+   .pipe(concat('crud.component.js'))
+   .pipe(gulp.dest('./dist/js/'));
+});
